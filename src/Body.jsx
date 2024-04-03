59f2a0d6eda044f91bcc8219b36e20f99da9d041
chumbah <116595613+chumbah@users.noreply.github.com>
2024-04-03 19:01:05 +0300

    Update Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Body(){
     return(
-        <div>
+        <div className="body-content">
                 <h2 className="head">About</h2>
                 <p className="abouts">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
                 <h2 className="head">Interests</h2>
