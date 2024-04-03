5d03ea3dafa8975d50fa8daaa9ef0417c03fc1e2
chumbah <116595613+chumbah@users.noreply.github.com>
2024-04-03 18:50:15 +0300

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,8 +7,8 @@ export default function Name(){
             <p className="skill">Front-end developer</p>
             <p className="web">alexkipchumba.website</p>
             <div className="link">
-            <button className="email"><a href="mailto:alexkebenei10@gmail.com?subject=Network with me=<b>ME</b>"></a> <img src="./src/assets/Icon.png"></img> Email</button>
-            <button className="linkedn"><a href="https://www.linkedin.com/in/alex-kipchumba-662717261/"></a><img src="./src/assets/Vector.png"></img> Linkedln</button>
+            <button className="email"><a href="mailto:alexkebenei10@gmail.com?subject=Network with me=<b>ME</b>"></a> <img src="/Icon.png"></img> Email</button>
+            <button className="linkedn"><a href="https://www.linkedin.com/in/alex-kipchumba-662717261/"></a><img src="/Vector.png"></img> Linkedln</button>
             </div>
         </div>
     )
