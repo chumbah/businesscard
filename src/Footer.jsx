fb1f8c5448dd3d92ef3eb0560b9671339f447aef
chumbah <116595613+chumbah@users.noreply.github.com>
2024-04-03 18:55:42 +0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer(){
     return(
         <div className="footer">
-            <a href="#"><img src="./src/assets/Facebook Icon.png"></img></a>
-            <a href="https://github.com/chumbah"><img src="./src/assets/GitHub Icon.png"></img></a>
-            <a href="https://www.instagram.com/aplus_dev?igsh=am04NG12cHA3eXUw"><img src="./src/assets/Instagram Icon.png"></img></a>
-            <a href="https://x.com/AlecChumbah?s=09"><img src="./src/assets/Twitter Icon.png"></img></a>
+            <a href="#"><img src="/Facebook Icon.png"></img></a>
+            <a href="https://github.com/chumbah"><img src="/GitHub Icon.png"></img></a>
+            <a href="https://www.instagram.com/aplus_dev?igsh=am04NG12cHA3eXUw"><img src="/Instagram Icon.png"></img></a>
+            <a href="https://x.com/AlecChumbah?s=09"><img src="/Twitter Icon.png"></img></a>
         </div>
     )
 }
