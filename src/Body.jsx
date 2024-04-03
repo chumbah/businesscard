60777454ac4e9481fcc90163a84189e0e504f5bc
chumbah <116595613+chumbah@users.noreply.github.com>
2024-04-03 18:48:44 +0300

    img2

--- src/Body.jsx
+++ src/Body.jsx
@@ -11,10 +11,10 @@ export default function Body(){
                 <p className="abouts">Expert front-end developer excelent in React, Javascript, HTML and CSS, crafting seamless user experiences with finesse.</p>
                 <ul>
                     <div className="skill-lst">
-                   <a href="www.react.com"><li>React<img src="/js.png"></img></li></a>
-                    <a href="www.javascript.com"><li>Javascript<img src="./src/assets/js.png"></img></li></a>
-                    <a href="www.html.com"><li>Html & css<img src="./src/assets/hc.png"></img></li></a>
-                    <a href="www.figma.com"><li>Figma designs<img src="./src/assets/figma.png"></img></li></a>
+                   <a href="www.react.com"><li>React<img src="/react.png"></img></li></a>
+                    <a href="www.javascript.com"><li>Javascript<img src="/js.png"></img></li></a>
+                    <a href="www.html.com"><li>Html & css<img src="/hc.png"></img></li></a>
+                    <a href="www.figma.com"><li>Figma designs<img src="/figma.png"></img></li></a>
                     </div>
                     </ul>    
         </div>
