d2c6f29f67130ff3130a2e3bd62575634fb137ce
chumbah <116595613+chumbah@users.noreply.github.com>
2024-04-03 18:49:14 +0300

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
     return(
         <nav className="header">
         <div className="header-pic">
-        <img src="./src/assets/user2.png"></img>
+        <img src="/user2.png"></img>
         <h3 className="hcontent">Digital business card</h3>
         </div>
         </nav>
