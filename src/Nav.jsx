a7d81b289bb715634f1bbcae5295229838242bbe
chumbah <116595613+chumbah@users.noreply.github.com>
2024-04-03 18:52:11 +0300

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ export default function Name(){
         <div>
             <h1 className="name">ALEX KIPCHUMBA</h1>
             <p className="skill">Front-end developer</p>
-            <p className="web">alexkipchumba.website</p>
+            <p className="web">https://businesscard-one.vercel.app/</p>
             <div className="link">
             <button className="email"><a href="mailto:alexkebenei10@gmail.com?subject=Network with me=<b>ME</b>"></a> <img src="/Icon.png"></img> Email</button>
             <button className="linkedn"><a href="https://www.linkedin.com/in/alex-kipchumba-662717261/"></a><img src="/Vector.png"></img> Linkedln</button>
